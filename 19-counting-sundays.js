f3efca63cf25215d8db73c7e6a5d01c3e6c4ca45
{{user_full_name}} <{{user_email}}>
2017-12-19 19:23:52 -0500

    cleaned up sol to be more legible.

--- 19-counting-sundays.js
+++ 19-counting-sundays.js
@@ -1,4 +1,23 @@
 /**
+ * @function range
+ *
+ */
+function* range(start, end, inclusive=1) {
+  let [s, e] = [start, end].map(x => Number.parseInt(x));
+  while (s < (e + inclusive))
+    yield s++;
+}
+
+/**
+ * @function isLeapYear
+ * @desc leap years are defined as divisible by 4, 400, but not 100
+ */
+function isLeapYear(year) {
+  return (year % 4 === 0) && ((year % 100 !== 0) || (year % 400 === 0));
+}
+
+
+/**
  * @function sundayMonthsInRange
  * @see https://projecteuler.net/problem=19
  * @author Jason Seminara <jseminara@gmail.com>
@@ -10,38 +29,31 @@
  * How many Sundays fell on the first of the month
     during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
  */
+
 function sundayMonthsInRange(start, end, offset=1) {
+  const sundays = [];
   let day = 0;
-  const monthLengths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-  /**
-   * @function isLeapYear
-   * @desc leap years are defined as divisible by 4, 400, but not 100
-   */
-  const isLeapYear = year =>
-    (year % 4 === 0) && ((year % 100 !== 0) || (year % 400 === 0));
+  // loop over the years
+  for (let year of range(start, end)) {
+    /* loop over the months and find the start of the month */
+    [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31].forEach((dayCount, m) => {
 
+      // Sundays occur every 7 days after the first known occurance
+      // We'll move the day over by 'offset', depending on what the given day was
+      // e.g. 1900-01-01 was a Monday, so the offset is 1;
+      if ((day + offset) % 7 === 0) {
+        sundays.push(day);
+      }
 
-  return [...Array(end - start + 1)].map((_, y) =>
-    monthLengths.map((days, m) => {
-      const monthDays = days + 0 + (isLeapYear(start + y) && m === 1);
       // increment by the month offset
-      day += monthDays;
-      // rewind to get back to the first of the month
-      return day - monthDays;
+      day += dayCount + (isLeapYear(year) && m === 1);
     })
-      /* check to see if this day is divisible by 7 */
-      .filter((day) => (day + offset) % 7 === 0)
-  )
-    /* we've included 1900 to count properly, but we don't want it in our results */
-    .slice(1)
+  }
 
-    /* collapse all sundays into one list and count them */
-    .reduce((allSundays, month) => {
-      return [...allSundays, ...month]
-    }, [])
-    .length();
+  /* we've included 1900 to count properly, but we don't want it in our results */
+  return sundays.filter(day => day > 364).length;
 }
 
 
-
+console.log(sundayMonthsInRange(1900, 2000));
